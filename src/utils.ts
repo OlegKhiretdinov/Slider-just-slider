c1c8c07cdecf522236b933ed93716895cc9ee59f
Oleg Khiretdinov <oleg@oleg.win>
2020-09-08 16:12:44 +0300

    add utils.ts and function for normalize options

---
+++ src/utils.ts
@@ -0,0 +1,21 @@
+export function normalaizeOptions(options:any):any {
+  const {
+    inputSelector = 'input',
+    sliderSelector = '.just-slider',
+    values = [0, 1000],
+    points = [100],
+    
+  } = options;
+
+  const inputEl:HTMLElement = document.querySelector(inputSelector);
+  const blockForSlider:HTMLElement = document.querySelector(sliderSelector);
+  const valueIsNumber = (values) => {
+    if (values.length !== 2) return false;
+    if (typeof values[0] !== 'number' || typeof(values[1]) !== 'number') return false;
+    return true;
+  }
+  const diapason = valueIsNumber(values) ? values: [0, values.length - 1];
+  points.sort((a, b) => a - b);
+
+  return {inputEl, blockForSlider, diapason, points};
+}
